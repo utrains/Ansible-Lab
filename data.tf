8851cc2783c200720b873bae7c79ac194f6a2a27
guilene1 <guilene.tiako@utrains.org>
2025-12-22 17:11:00 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -15,6 +15,14 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami*x86_64"]
+  }
+}
 
 data "aws_ami" "amazon_linux2" {
   most_recent = true
